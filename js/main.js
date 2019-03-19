104e6f9031735dff09129242bf61313390f08f8b
KhanX <m90khan@gmail.com>
2019-03-20 00:05:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,15 +10,11 @@ function game(){
   var image1 =  document.querySelectorAll("img")[0].setAttribute("src", dice1);
    //and change its src attribute to random
   // image1.setAttribute("src", dice1);
-
   var d2  = Math.floor(Math.random() * 6) +1;
   var dice2 = "images/dice" + d2 + ".png";
-
   var image2 = document.querySelectorAll("img")[1].setAttribute("src", dice2);
   document.querySelector("#tied").classList.add("truth-dare");
   document.querySelector("#outputtruth").classList.add("truth-dare");
-
-
 // Condition on who wins
 // When Player 1 wins
 if (d1 > d2) {
@@ -34,13 +30,11 @@ document.querySelector("#tied").classList.add("truth-dare");
   document.querySelector("#playme").classList.add("truth-dare");
   document.querySelector("#truther").classList.toggle("truth-dare");
   document.querySelector("#tied").classList.add("truth-dare");
-
    // // When it is a Tie
 }else{
   document.querySelector("#tied").style.backgroundColor = "white";
   document.querySelector("#tied").classList.toggle("truth-dare");
   document.querySelector("#tied").innerHTML = "Oh No: It`s a Tie <br> Both of you Drink a shot and Play Again";
-
   document.querySelectorALL("output1").style.visibility = "hidden";
 
 
@@ -48,10 +42,7 @@ document.querySelector("#tied").classList.add("truth-dare");
 
 
 }
-
-
-function dareQuestion(){
-   //                                             // Dare
+  //                                             // Dare
    var diceArray4 = [
      "For a guy, put on makeup. For a girl, wash off your make up.",
      "Dance with no music for 1 minute.",
@@ -70,9 +61,8 @@ function dareQuestion(){
      "Remove whatever you wearing as a Top",
      "If you are at his/her place, wear his/her clothes",
      ];
-
-
-    var d4 = Math.random();
+function dareQuestion(){
+     var d4 = Math.random();
     d4 = d4 * diceArray4.length;
     d4 = Math.floor(d4)+1;
     var dice4 = diceArray4[d4];
@@ -81,20 +71,8 @@ function dareQuestion(){
       document.querySelector("#truther").classList.add("truth-dare");
       document.querySelector("#playme").classList.toggle("truth-dare");
       document.querySelector("#outputtruth").classList.toggle("truth-dare");
-
-
-
     }
-   //   var d4 = Math.floor(Math.random() * diceArray4.length) +1;
-   // var dice4 = diceArray3[d3];
-   // if (isNaN (dic4)){
-   //   document.querySelector("#outputtruth").innerHTML = dice4;
-   // }
-   // document.querySelector("#playme").classList.toggle("truth-dare");
-   // document.querySelector("#truther").classList.toggle("truth-dare");
 }
-//
-function truthQuestion(){
    //                                               // Truth
    var diceArray3 = [
      "What is the most childish thing you still do?",
@@ -113,6 +91,8 @@ function truthQuestion(){
      "What memory do you just keep going back to?",
      "Where is the dream place you wish to visit?",
      ];
+//
+function truthQuestion(){
      var d3 = Math.random();
     d3 = d3 * diceArray3.length;
     d3 = Math.floor(d3)+1;
@@ -122,16 +102,5 @@ function truthQuestion(){
       document.querySelector("#truther").classList.toggle("truth-dare");
       document.querySelector("#playme").classList.toggle("truth-dare");
       document.querySelector("#outputtruth").classList.toggle("truth-dare");
-
     }
-
-
-   //   var d3 = Math.floor(Math.random() * diceArray3.length) +1;
-   // var dice3 = diceArray3[d3];
-   // if (isNaN (dic3)){
-   //   document.querySelector("#outputtruth").innerHTML = dice3;
-   // }
-   // document.querySelector("#playme").classList.toggle("truth-dare");
-   // document.querySelector("#truther").classList.toggle("truth-dare");
-
 }
